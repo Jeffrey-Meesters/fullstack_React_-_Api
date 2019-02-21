a3763b1ebc1a59f7da1a2305703cef227f0cc2ac
Jeffrey Meesters <jjeffreymeesters@gmail.com>
2019-02-21 20:28:28 +0100

    Add all hardcoded html from the markup example

--- client/src/elements/Header.js
+++ client/src/elements/Header.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <h1>Header</h1>
+            <div class="header">
+                <div class="bounds">
+                    <h1 class="header--logo">Courses</h1>
+                    <nav>
+                        <a class="signup" href="sign-up.html">Sign Up</a>
+                        <a class="signin" href="sign-in.html">Sign In</a>
+                    </nav>
+                </div>
+            </div>
         )
     }
 }
